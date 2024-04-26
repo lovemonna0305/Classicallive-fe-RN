0ae004da662c4a49ae403fbf905808ec33ff9508
lovemonna0305 <lovemonna0305@gmail.com>
2024-04-26 02:27:05 -0700

    implement video streaming

---
+++ ios/VideosdkPRK.m
@@ -0,0 +1,15 @@
+//
+//  VideosdkPRK.m
+//  classicallive
+//
+//  Created by admin on 4/25/24.
+//
+
+#import <Foundation/Foundation.h>
+#import <React/RCTBridgeModule.h>
+#import "React/RCTEventEmitter.h"
+
+@interface RCT_EXTERN_MODULE(VideosdkRPK, RCTEventEmitter)
+RCT_EXTERN_METHOD(startBroadcast)
+
+@end
--- src/scenes/ILS/Speaker/MeetingViewer.js
+++ src/scenes/ILS/Speaker/MeetingViewer.js
@@ -149,6 +149,7 @@ export default function MeetingViewer({setlocalParticipantMode}) {
       });
 
       return () => {
+        // VideosdkRPK.removeSubscription('onScreenShare');
         VideosdkRPK.removeAllListeners('onScreenShare');
       };
     }
